43000cb8051b3bbab4341d7baf9ffe3d7d2169b4
Ekasit Jarussinvichai <ekasit.ja@gmail.com>
2016-02-08 11:40:04 -0800

    testing making claims pages

---
+++ claims/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+
+from infoGatherer.models import Personal_Information, Payer
+from fdfgen import forge_fdf
+import os
+
+# Create your views here.
+def index(request):
+    return render(request, 'claims/index.html')
+
+def createClaim(request):
+    patient_list = Personal_Information.objects.order_by('first_name')
+    payer_list = Payer.objects.order_by('name')
+    context = {
+        'patient_list': patient_list,
+        'payer_list': payer_list,
+    }
+
+    return render(request, 'claims/createClaim.html', context)
+
+def generateClaim(request):
+    if request.method == 'POST' and \
+        request.POST['patient_chart_no'] and \
+        request.POST['insured_chart_no'] and \
+        request.POST['payer_code']:
+
+        patient = get_object_or_404(Personal_Information, pk=request.POST['patient_chart_no'])
+        insured = get_object_or_404(Personal_Information, pk=request.POST['insured_chart_no'])
+        payer = get_object_or_404(Payer, pk=request.POST['payer_code'])
+        fields = [
+            ('2', payer.address),
+            ('10', patient.first_name + ' ' + patient.last_name),
+            ('11', insured.first_name + ' ' + insured.last_name),
+        ]
+        fdf = forge_fdf("",fields,[],[],[])
+        fdf_file = open("data.fdf","w")
+        fdf_file.write(fdf)
+        fdf_file.close()
+
+        os.system('pdftk CMS1500.pdf fill_form data.fdf output output.pdf')
+        os.remove('data.fdf')
+
+        with open('output.pdf', 'rb') as pdf:
+            response = HttpResponse(pdf.read(), content_type='application/pdf')
+            response['Content-Disposition'] = 'inline;filename=output.pdf'
+            return response
+        pdf.closed
+
+    else:
+
+        return render(request, 'claims/generateClaim.html')
--- infoGatherer/urls.py
+++ infoGatherer/urls.py
@@ -1,25 +1,25 @@
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url, include
 from infoGatherer import views
 from django.contrib.auth.views import logout
-urlpatterns = patterns('',
 
-        url(r'^search-form/$', views.search_form),
-		url(r'^search-form/print/$', views.print_form),
-		
-        url(r'^$', views.index, name='index'),
-        url(r'^log$', views.admin_log, name='log'),
-        url(r'^login/$', views.user_login, name='login'),
-        url(r'^logout/$', views.user_logout, name='logout', kwargs={'next_page': '/info/login/'}),
-        url(r'^patient/new/$',views.get_patient_info),
-        url(r'^guarantor/(?P<id>\d+)/$',views.get_guarantor_info),
-        url(r'^insurance/(?P<id>\d+)/$',views.get_insurance_info),
-        url(r'^view/(?P<who>\w{3})/$',views.get_patient_info),
-        url(r'^view/(?P<who>\d+)/$',views.get_patient_info),
-#         url(r'^user/password/reset/$', views.user_password_reset, name="password_reset"),
-#         url(r'^user/password/reset/done/$',views.user_password_reset_done),
-#         url(r'^user/password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', views.user_password_reset_confirm, name='password_reset_confirm'),
-#         url(r'^user/password/done/$', views.user_password_reset_complete),
+urlpatterns = [
+    url(r'^search-form/$', views.search_form),
+    url(r'^search-form/print/$', views.print_form),
+
+    url(r'^$', views.index, name='index'),
+    url(r'^log$', views.admin_log, name='log'),
+    url(r'^login/$', views.user_login, name='login'),
+    url(r'^logout/$', views.user_logout, name='logout', kwargs={'next_page': '/info/login/'}),
+    url(r'^patient/new/$',views.get_patient_info),
+    url(r'^guarantor/(?P<id>\d+)/$',views.get_guarantor_info),
+    url(r'^insurance/(?P<id>\d+)/$',views.get_insurance_info),
+    url(r'^view/(?P<who>\w{3})/$',views.get_patient_info),
+    url(r'^view/(?P<who>\d+)/$',views.get_patient_info),
+    #url(r'^user/password/reset/$', views.user_password_reset, name="password_reset"),
+    #url(r'^user/password/reset/done/$',views.user_password_reset_done),
+    #url(r'^user/password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', views.user_password_reset_confirm, name='password_reset_confirm'),
+    #url(r'^user/password/done/$', views.user_password_reset_complete),
 
 
 
-        )
+]
