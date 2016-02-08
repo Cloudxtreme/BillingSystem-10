327356ca4e7c2239bbf566b757fd696a0704148e
Ekasit Jarussinvichai <ekasit.ja@gmail.com>
2016-02-08 13:09:49 -0800

    debug urls.py in infoGatherer

--- infoGatherer/urls.py
+++ infoGatherer/urls.py
@@ -1,11 +1,13 @@
-from django.conf.urls import url, include
+from django.conf.urls import patterns, url, include
 from infoGatherer import views
 from infoGatherer.views import PostAdPage
 from django.contrib.auth.views import logout
 
-urlpatterns = [
+urlpatterns = patterns('',
+    (r'^postad/', PostAdPage.as_view()),
     url(r'^search-form/$', views.search_form),
     url(r'^search-form/print/$', views.print_form),
+    url(r'^claim/$', views.view_in_between),
 
     url(r'^$', views.index, name='index'),
     url(r'^log$', views.admin_log, name='log'),
@@ -16,11 +18,11 @@ urlpatterns = [
     url(r'^insurance/(?P<id>\d+)/$',views.get_insurance_info),
     url(r'^view/(?P<who>\w{3})/$',views.get_patient_info),
     url(r'^view/(?P<who>\d+)/$',views.get_patient_info),
-    #url(r'^user/password/reset/$', views.user_password_reset, name="password_reset"),
-    #url(r'^user/password/reset/done/$',views.user_password_reset_done),
-    #url(r'^user/password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', views.user_password_reset_confirm, name='password_reset_confirm'),
-    #url(r'^user/password/done/$', views.user_password_reset_complete),
+#         url(r'^user/password/reset/$', views.user_password_reset, name="password_reset"),
+#         url(r'^user/password/reset/done/$',views.user_password_reset_done),
+#         url(r'^user/password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', views.user_password_reset_confirm, name='password_reset_confirm'),
+#         url(r'^user/password/done/$', views.user_password_reset_complete),
 
 
 
-]
+)
