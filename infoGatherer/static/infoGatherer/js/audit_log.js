8ebc22ceac2765aeb369f3ddd8ccdb9282169549
Utsav <utsav26@gmail.com>
2016-03-18 12:16:51 -0700

    dropdown

--- infoGatherer/static/infoGatherer/js/audit_log.js
+++ infoGatherer/static/infoGatherer/js/audit_log.js
@@ -36,6 +36,30 @@ $(document).ready(function(){
 
 });
 
+$(document).ready(function(){
+    $('#patienthref').on('click', function(){
+        $("#fillMe").html(replaceMe("patient"));
+    });
+    $('#payerhref').on('click', function(){
+        $("#fillMe").html(replaceMe("payer"));
+    });
+    $('#insurancehref').on('click', function(){
+        $("#fillMe").html(replaceMe("insurance"));
+    });
+    $('#providerhref').on('click', function(){
+        $("#fillMe").html(replaceMe("provider"));
+    });
+    $('#rphref').on('click', function(){
+        $("#fillMe").html(replaceMe("rp"));
+    });
+    $('#cpthref').on('click', function(){
+        $("#fillMe").html(replaceMe("cpt"));
+    });
+    $('#dxhref').on('click', function(){
+        $("#fillMe").html(replaceMe("dx"));
+    });
+});
+
 
 $(document).ready(function(){
     var this_js_script = $('script[src*=audit_log]');
@@ -65,6 +89,22 @@ $(document).ready(function(){
         }
     }
 });
+
+function replaceMe(ele){
+    var dump = '<a href="#" data-toggle="dropdown"><strong id="showEntriesNav">Show 10 Entries</strong><span class="caret"></span></a>'+
+                                '                    <ul class="dropdown-menu" role="menu">'+
+                                '                        <li><a href="?num_m=5&num_c=5&num_d=5&'+ele+'=1">5</a></li>'+
+                                '                        <li><a href="?num_m=10&num_c=10&num_d=10&'+ele+'=1">10 (default)</a></li>'+
+                                '                        <li><a href="?num_m=25&num_c=25&num_d=15&'+ele+'=1">25</a></li>'+
+                                '                        <li><a href="?num_m=all&num_c=all&num_d=all&'+ele+'=1">All</a></li>'+
+                                '                    </ul>';
+    return dump
+}
+
+
+
+
+
 
 // $(document).ready(function(){
 //     var this_js_script = $('script[src*=audit_log]');
