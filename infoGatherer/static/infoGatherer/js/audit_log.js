0da53df0552a2b0f3ff4fb85e5d69d17a0b276b0
Utsav <utsav26@gmail.com>
2016-03-18 16:45:14 -0700

    b4 search

--- infoGatherer/static/infoGatherer/js/audit_log.js
+++ infoGatherer/static/infoGatherer/js/audit_log.js
@@ -1,14 +1,40 @@
 
 
+
+function modCreDel(){
+    if($("#mod").hasClass("active")){
+        return 'mod';
+    }
+    else if($("#del").hasClass("active")){
+        return 'del';
+    }
+    else if($("#cre").hasClass("active")){
+        return 'cre';
+    }
+    
+};
+
+
+
+
 $(document).ready(function(){
-    $(function(){
-        var this_js_script = $('script[src*=audit_log]');
-        $(".m").children(".btn-group").children(".dropButton").html(this_js_script.attr('row_m')+" Entries");
-        $(".c").children(".btn-group").children(".dropButton").html(this_js_script.attr('row_c')+" Entries");
-        $(".d").children(".btn-group").children(".dropButton").html(this_js_script.attr('row_d')+" Entries");
+    var this_js_script = $('script[src*=audit_log]');
+    $("#mod").click(function(){
+        $("#fillMe").html(replaceMe(this_js_script.attr('display'),"mod",this_js_script.attr('row_m')));
+    });
+    $("#cre").click(function(){
+        console.log("213123");
+        $("#fillMe").html(replaceMe(this_js_script.attr('display'),"cre",this_js_script.attr('row_m')));
+    });
+    $("#del").click(function(){
+        $("#fillMe").html(replaceMe(this_js_script.attr('display'),"del",this_js_script.attr('row_m')));
     });
 
 });
+
+
+
+
 
 
 $(document).ready(function(){
@@ -20,32 +46,37 @@ $(document).ready(function(){
 
 $(document).ready(function(){
     var this_js_script = $('script[src*=audit_log]');
+
+    // $("#showEntriesNav").html(showEntry(this_js_script.attr('row_m')));
+
+
     $('#patienthref').on('click', function(){
-        $("#fillMe").html(replaceMe("patient"));
+        $("#fillMe").html(replaceMe("patient",modCreDel(),this_js_script.attr('row_m')));
     });
     $('#payerhref').on('click', function(){
-        $("#fillMe").html(replaceMe("payer"));
+        $("#fillMe").html(replaceMe("payer",modCreDel(),this_js_script.attr('row_m')));
     });
     $('#insurancehref').on('click', function(){
-        $("#fillMe").html(replaceMe("insurance"));
+        $("#fillMe").html(replaceMe("insurance",modCreDel(),this_js_script.attr('row_m')));
     });
     $('#providerhref').on('click', function(){
-        $("#fillMe").html(replaceMe("provider"));
+        $("#fillMe").html(replaceMe("provider",modCreDel(),this_js_script.attr('row_m')));
     });
     $('#rphref').on('click', function(){
-        $("#fillMe").html(replaceMe("rp"));
+        $("#fillMe").html(replaceMe("rp",modCreDel(),this_js_script.attr('row_m')));
     });
     $('#cpthref').on('click', function(){
-        $("#fillMe").html(replaceMe("cpt"));
+        $("#fillMe").html(replaceMe("cpt",modCreDel(),this_js_script.attr('row_m')));
     });
     $('#dxhref').on('click', function(){
-        $("#fillMe").html(replaceMe("dx"));
+        $("#fillMe").html(replaceMe("dx",modCreDel(),this_js_script.attr('row_m')));
     });
 
     // Replace text in page dropdown
-    $('.changeDropdownContent').on('click', function(){
-        $("#showEntriesNav").html(showEntry(this_js_script.attr('row_m')));
-    });
+
+    // $('.changeDropdownContent').on('click', function(){
+    //     $("#showEntriesNav").html(showEntry(this_js_script.attr('row_m')));
+    // });
     
 });
 
@@ -79,17 +110,37 @@ $(document).ready(function(){
     }
 });
 
+
+
+
+$(document).ready(function(){
+    var this_js_script = $('script[src*=audit_log]');
+    if(typeof this_js_script.attr('typemcd') != 'undefined'){
+        var display = this_js_script.attr('typemcd');
+        console.log(display);
+        if(display.localeCompare("mod")==0){
+            $( "#mod a" ).trigger( "click" );
+        }
+        else if(display.localeCompare("cre")==0){
+            $( "#cre a" ).trigger( "click" );
+        }
+        else if(display.localeCompare("del")==0){
+            $( "#del a" ).trigger( "click" );
+        }
+    }
+});
+
 function showEntry(ele){
     return 'Show '+ele+' Entries';
 }
 
-function replaceMe(ele){
-    var dump = '<a href="#" data-toggle="dropdown"><strong id="showEntriesNav">Show 10 Entries</strong><span class="caret"></span></a>'+
+function replaceMe(ele, typemcd, entries){
+    var dump = '<a href="#" data-toggle="dropdown"><strong id="showEntriesNav">Show '+entries+' Entries</strong><span class="caret"></span></a>'+
                                 '                    <ul class="dropdown-menu" role="menu">'+
-                                '                        <li><a href="?num_m=5&num_c=5&num_d=5&'+ele+'=1">5</a></li>'+
-                                '                        <li><a href="?num_m=10&num_c=10&num_d=10&'+ele+'=1">10 (default)</a></li>'+
-                                '                        <li><a href="?num_m=25&num_c=25&num_d=15&'+ele+'=1">25</a></li>'+
-                                '                        <li><a href="?num_m=all&num_c=all&num_d=all&'+ele+'=1">All</a></li>'+
+                                '                        <li><a href="?num_m=5&num_c=5&num_d=5&'+'typemcd='+typemcd+'&'+ele+'=1">5</a></li>'+
+                                '                        <li><a href="?num_m=10&num_c=10&num_d=10&'+'typemcd='+typemcd+'&'+ele+'=1">10 (default)</a></li>'+
+                                '                        <li><a href="?num_m=25&num_c=25&num_d=15&'+'typemcd='+typemcd+'&'+ele+'=1">25</a></li>'+
+                                '                        <li><a href="?num_m=all&num_c=all&num_d=all&'+'typemcd='+typemcd+'&'+ele+'=1">All</a></li>'+
                                 '                    </ul>';
     return dump
 }
@@ -97,45 +148,4 @@ function replaceMe(ele){
 
 
 
-
-
-// $(document).ready(function(){
-//     var this_js_script = $('script[src*=audit_log]');
-//     var display_rows = this_js_script.attr('datamy');
-    
-//     function removeClassLi(){
-//         $("#_5").removeClass("active");
-//         $("#_10").removeClass("active");
-//         $("#_25").removeClass("active");
-//         $("#_100").removeClass("active");
-//     }
-//     $(function(){
-//         var i=display_rows;
-//         if(i==5){
-//             $(".page-size").html("5");
-//             removeClassLi();
-//             $("#_5").addClass("active");
-//         }
-//         else if(i==10){
-//             $(".page-size").html("10");
-//             removeClassLi();
-//             $("#_10").addClass("active");
-//         }
-//         else if(i==25){
-//             $(".page-size").html("25");
-//             removeClassLi();
-//             $("#_25").addClass("active");
-//         }else if(i==100){
-//             $(".page-size").html("100");
-//             removeClassLi();
-//             $("#_100").addClass("active");
-//         }else{
-//             $(".page-size").html("10");
-//             removeClassLi();
-//             $("#_10").addClass("active");
-//         }
-
-//     });
-
-// });
 
