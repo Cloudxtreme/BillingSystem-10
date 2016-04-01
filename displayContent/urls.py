b66a1624a7f6b0cc8758541fe2bdd95e5f360107
Utsav <utsav26@gmail.com>
2016-04-01 15:18:12 -0700

    progress on claims

--- displayContent/urls.py
+++ displayContent/urls.py
@@ -5,8 +5,8 @@ from django.contrib.auth.views import logout
 app_name ='displayContent'
 urlpatterns = [
     url(r'^dashboard/$', views.view_dashboard, name="view_dashboard"),
-    url(r'^[0-9]+/claimhistory/$', views.view_claims, name="view_claims"),
-    url(r'^[0-9]+/$', views.view_patient, name="view_patient"),
+    url(r'^(?P<chart>\w+)/claimhistory/$', views.view_claims, name="view_claims"),
+    url(r'^(?P<chart>\w+)/$', views.view_patient, name="view_patient"),
 
     # url(r'^payment/apply/(?P<payment_id>\w+)/(?P<claim_id>\w+)/patient/$', views.charge_patient_create, name="charge_patient_create"),
 
--- displayContent/views.py
+++ displayContent/views.py
@@ -10,35 +10,40 @@ from accounting.models import Claim, Charge, Procedure
 from django.http import JsonResponse
 from base.models import ExtPythonSerializer
 import re
+from decimal import *
 
 
 def view_dashboard(request):
     return render(request, 'displayContent/dashboard.html')
 
-def view_claims(request):
-    m = re.search('.*patient\/([0-9]+)\/.*', str(request))
-    chart=m.group(1)
+def view_claims(request, chart):
+    """
+    Renders the /patient/<no>/claimhistory webpage. Information is used for claim detals and claim searches (go to url).
+    """
+    patient_info=Personal_Information.objects.filter(pk=chart).values()
+    claimSearches=Claim.objects.filter(patient_id=chart)
+    if len(patient_info)>0:
+        claimValues=[]
+        claimValues=claimSearches.values()[:]
 
-    obj=Personal_Information.objects.filter(pk=chart)
-    patient_info=obj.values()
-    claims=Claim.objects.filter(patient_id=chart).values()
-    for claim in claims:
+        for claim, claimValue in zip(claimSearches, claimValues):
+            claimValue["total_charge"]=claim.total_charge
+            claimValue["ins_pmnt"]=claim.ins_pmnt_per_claim
+            claimValue["ins_adjustment"]=claim.ins_adjustment_per_claim
+            claimValue["pat_responsible"]=claim.pat_responsible_per_claim
+            claimValue["pat_pmnt"]=claim.pat_pmnt_per_claim
+            claimValue["inc_balance"]=Decimal(claim.total_charge)-Decimal(claim.ins_pmnt_per_claim+claim.ins_adjustment_per_claim)
+            claimValue["total_balance"]=claimValue["inc_balance"]+(claim.pat_responsible_per_claim-claim.pat_pmnt_per_claim)
 
-        claim["total_charge"]=Charge.objects.filter(procedure__claim=claim["id"])\
-                                .aggregate(Sum('amount'))['amount__sum'] or 0
+        return render(request, 'displayContent/patient/claim.html',{
+            'claimSearches' : claimValues,
+            'patient_info' : patient_info[0],
+        })
+    else:
+        return HttpResponse("<html><h2>The patientID doesn't exist in the database.</h2></html>")
 
-    return render(request, 'displayContent/patient/claim.html',{
-        'claims' : claims,
-        'patient_info' : patient_info[0],
-    })
-
-
-def view_patient(request):
+def view_patient(request, chart):
     """Page to view info about patients and goto the claims"""
-
-    m = re.search('.*patient\/([0-9]+)\/.*', str(request))
-    chart=m.group(1)
-
     obj=Personal_Information.objects.filter(pk=chart)
     patient_info=obj.values()
 
@@ -81,41 +86,6 @@ def view_patient(request):
     else:
         return JsonResponse([], safe=False)
 
-
-def api_payment_summary(request):
-    if request.method == 'POST':
-        post_data = request.POST
-
-        if post_data.get('claim_id'):
-            claim = Claim.objects.filter(pk__icontains=post_data.get('claim_id'))
-            se = ExtPythonSerializer().serialize(
-                claim,
-                use_natural_foreign_keys=True
-            )
-            return JsonResponse(data=se, safe=False)
-
-        else:
-            patient = Personal_Information.objects.filter(
-                first_name__icontains=post_data.get('first_name') or '',
-                last_name__icontains=post_data.get('last_name') or '',
-            )
-            if post_data.get('dob'):
-                patient = patient.filter(dob=post_data.get('dob'))
-            if post_data.get('phone'):
-                patient = patient.filter(home_phone__icontains=post_data.get('phone'))
-            if post_data.get('ssn'):
-                patient = patient.filter(ssn__icontains=post_data.get('ssn'))
-            if post_data.get('chart_no'):
-                patient = patient.filter(chart_no__icontains=post_data.get('chart_no'))
-            se = ExtPythonSerializer().serialize(
-                patient,
-                props=['chart_no','get_primary_insurane',],
-                use_natural_foreign_keys=True
-            )
-            return JsonResponse(data=se, safe=False)
-    else:
-        return JsonResponse([], safe=False)
-        
 
 def api_search_patient(request):
     if request.method == 'POST':
