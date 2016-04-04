7d62dc4d724864e1d36ac2522b2d397c1472e48c
Ekasit Jarussinvichai <ekasit.ja@gmail.com>
2016-04-04 09:40:51 -0700

    -custom css -change url to capture only digit instead of whole word

--- displayContent/urls.py
+++ displayContent/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^(?P<chart>\d+)/claimhistory/$', views.view_claims, name="view_claims"),
     url(r'^api_search_patient/$', views.api_search_patient, name="api_search_patient"),
 
-    url(r'^(?P<claim_id>\w+)/payment/$', views.payment_details, name="payment_details"),
+    url(r'^(?P<claim_id>\d+)/payment/$', views.payment_details, name="payment_details"),
 
-    
+
 ]
