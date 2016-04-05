e3dc945668f13617a93b67f14051fabf93ff9673
Utsav <utsav26@gmail.com>
2016-04-04 17:26:44 -0700

    finsihed with saving claim.pdf

--- displayContent/models.py
+++ displayContent/models.py
@@ -1,8 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from accounting.models import Claim
 
 # Create your models here.
 
-class Document(models.Model):
-    docfile = models.FileField(upload_to='documents/%Y/%m/%d')
