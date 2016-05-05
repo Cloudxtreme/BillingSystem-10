1ea63cc5c882e36fe22b8f9a339b43d45b922228
Ekasit Jarussinvichai <ekasit.ja@gmail.com>
2016-05-05 13:29:18 -0700

    merge small js file together

--- static/js/misc.js
+++ static/js/misc.js
@@ -1,3 +1,39 @@
+/* Include this file to force jQuery to include
+csrf token in header of every single ajax call */
+$(document).ready(function() {
+    function getCookie(cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for(var i = 0; i <ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length,c.length);
+            }
+        }
+        return "";
+    }
+
+    var csrftoken = getCookie("csrftoken");
+
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+});
+
+
+
 // Automatically add trailing two decimals for input type number
 $(document).ready(function() {
     $("input[type=number]").each(function() {
